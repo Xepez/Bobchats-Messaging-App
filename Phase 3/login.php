9b36e5c06ad17a31311d115d461353db4a9d2435
Xepez <Peyton.glynn@gmail.com>
2018-11-27 14:13:11 -0800

    fixed connection

--- Phase 3/login.php
+++ Phase 3/login.php
@@ -1,17 +1,41 @@
 <?php
-//include 'connect.php';
+    include 'test_con.php';
 
-if(empty($_POST['username'])) {
-    $this->HandleError("UserName is empty!");
-    return false;
-}
-if(empty($_POST['password'])) {
-    $this->HandleError("Password is empty!");
-    return false;
-}
-$username = trim($_POST['username']);
-$password = trim($_POST['password']);
+    connect();
+    
+    if(empty($_POST['firstname'])) {
+        $this->HandleError("First Name is empty!");
+        return false;
+    }
+    if(empty($_POST['lastname'])) {
+        $this->HandleError("Last Name is empty!");
+        return false;
+    }
+    $firstname = trim($_POST['firstname']);
+    $lastname = trim($_POST['lastname']);
+    
+    //echo $firstname;
+    //echo $lastname;
 
-echo $username;
-echo $password;    
+    // First Name Query
+    $username_query = $test_pdo->prepare("SELECT first_name FROM user WHERE first_name = ?");
+    $username_query->bindParam(1, $firstname);
+    $username_query->execute();
+    $first = $username_query->fetchColumn();
+    
+    // Last Name Query
+    $username_query = $test_pdo->prepare("SELECT first_name FROM user WHERE first_name = ?");
+    $username_query->bindParam(1, $firstname);
+    $username_query->execute();
+    $first = $username_query->fetchColumn();
+    
+    // Check Both Queries
+    if (first == null || last == null) {
+        $this->HandleError("No User With This Name");
+        return false
+    }
+    else {
+        echo "WORKED!";
+    }
+    
 ?>
